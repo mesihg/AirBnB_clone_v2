5befd61764c03bfa2e8e99302dd22164d245af8e
Messih Grmay <mesihgrmay@yahoo.com>
2022-10-30 12:30:45 -0700

    Task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Fabric script that distributes an archive to your web servers """
 
-from os.path import exists
+import os.path
 from fabric.api import put, run, env
 
 env.hosts = ['44.210.69.40', '100.25.135.77']
@@ -11,32 +11,33 @@ def do_deploy(archive_path):
     """
     copy archive_path file from local to remote webservers
     """
-
-    if not exists(archive_path):
+    if os.path.isfile(archive_path) is False:
         return False
-    try:
-        file_name = archive_path.split("/")[-1].split(".")[0]
-        put(archive_path, "/tmp/")
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
 
-        run("sudo mkdir -p /data/web_static/releases/{}".format(file_name))
-
-        run("sudo tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
-            .format(file_name, file_name))
-
-        run('sudo rm -rf /tmp/{}.tgz'.format(file_name))
-
-        run(('sudo mv /data/web_static/releases/{}/web_static/* ' +
-            '/data/web_static/releases/{}/')
-            .format(file_name, file_name))
-
-        run('sudo rm -rf /data/web_static/releases/{}/web_static'
-            .format(file_name))
-
-        run('sudo rm -rf /data/web_static/current')
-
-        run(('sudo ln -s /data/web_static/releases/{}/' +
-            ' /data/web_static/current')
-            .format(file_name))
-        return True
-    except Exception:
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
+    if run("sudo rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("sudo mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
+        return False
+    if run("sudo rm /tmp/{}".format(file)).failed is True:
+        return False
+    if run("sudo mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run("sudo rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
+        return False
+    if run("sudo rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
+        return False
+    return True
