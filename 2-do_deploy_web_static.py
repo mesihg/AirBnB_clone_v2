414bb498ab69b602d68f917d10212e529e709312
Messih Grmay <mesihgrmay@yahoo.com>
2022-10-30 13:52:04 -0700

    Task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,9 +8,7 @@ env.hosts = ['44.210.69.40', '100.25.135.77']
 
 
 def do_deploy(archive_path):
-    """
-    copy archive_path file from local to remote webservers
-    """
+    """Distribute archive_path file from local to remote webservers"""
     if os.path.isfile(archive_path) is False:
         return False
     file = archive_path.split("/")[-1]
@@ -18,26 +16,26 @@ def do_deploy(archive_path):
 
     if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
-    if run("sudo rm -rf /data/web_static/releases/{}/".
+    if run("rm -rf /data/web_static/releases/{}/".
            format(name)).failed is True:
         return False
-    if run("sudo mkdir -p /data/web_static/releases/{}/".
+    if run("mkdir -p /data/web_static/releases/{}/".
            format(name)).failed is True:
         return False
-    if run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
            format(file, name)).failed is True:
         return False
-    if run("sudo rm /tmp/{}".format(file)).failed is True:
+    if run("rm /tmp/{}".format(file)).failed is True:
         return False
-    if run("sudo mv /data/web_static/releases/{}/web_static/* "
+    if run("mv /data/web_static/releases/{}/web_static/* "
            "/data/web_static/releases/{}/".format(name, name)).failed is True:
         return False
-    if run("sudo rm -rf /data/web_static/releases/{}/web_static".
+    if run("rm -rf /data/web_static/releases/{}/web_static".
            format(name)).failed is True:
         return False
-    if run("sudo rm -rf /data/web_static/current").failed is True:
+    if run("rm -rf /data/web_static/current").failed is True:
         return False
-    if run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current".
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
            format(name)).failed is True:
         return False
     return True
