597611f7a52fb564e0b069a316fc3c40e67f43da
Messih Grmay <mesihgrmay@yahoo.com>
2022-10-30 10:40:22 -0700

    Task 2

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" Fabric script that distributes an archive to your web servers """
+
+from os.path import exists
+from fabric.api import put, run, env
+
+env.hosts = ['44.210.69.40', '100.25.135.77']
+
+
+def do_deploy(archive_path):
+    """
+    copy archive_path file from local to remote webservers
+    """
+
+    if not exists(archive_path):
+        return False
+    try:
+        file_name = archive_path.split("/")[-1].split(".")[0]
+        put(archive_path, "/tmp/")
+
+        run("mkdir -p /data/web_static/releases/{}".format(file_name))
+
+        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
+            .format(file_name, file_name))
+
+        run('rm -rf /tmp/{}.tgz'.format(file_name))
+
+        run(('mv /data/web_static/releases/{}/web_static/* ' +
+            '/data/web_static/releases/{}/')
+            .format(file_name, file_name))
+
+        run('rm -rf /data/web_static/releases/{}/web_static'
+            .format(file_name))
+
+        run('rm -rf /data/web_static/current')
+
+        run(('ln -s /data/web_static/releases/{}/' +
+            ' /data/web_static/current')
+            .format(file_name))
+        return True
+    except Exception:
+        return False
